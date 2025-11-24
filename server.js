08eaa3794ac9fc083321a1265375cb5752e900a0
kuklas <skukla@redhat.com>
2025-11-24 15:00:16 +0100

    Add COST-5691 User Configurable Thresholds prototype to Backlog

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 // Serve static files from each prototype's dist folder
 const prototypes = [
   'COST-3353-ReportBuilder',
+  'COST-5691-UserConfigurableThresholds',
   'COST-6922-GoogleCloudInvoiceMonthCostPerspective',
   'COST-6881-AddingArchitectureForOCPNodeAndClusters',
   'COST-6951-CostTiers',
